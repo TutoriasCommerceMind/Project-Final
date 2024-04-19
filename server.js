c3aab441fb6d9a0965d425bd2b1e1836d42a97ac
TutoriasCommerceMind <149403750+TutoriasCommerceMind@users.noreply.github.com>
2024-04-18 22:20:56 -0300

    Trabajo finalizado , agregamos JWT

--- server.js
+++ server.js
@@ -1,10 +1,15 @@
-// Importamos express y las rutas de usuario y la conexion a la db.
+// Importamos express  y la conexion a la db.
 
 const express = require("express")
-const userRoutes = require("./routes/userRoutes")
 const connectDb = require("./db/db")
 
 // Creamos una instancia de Express.
+
+// Importamos las rutas
+
+const userRoutes = require("./routes/userRoutes")
+const authRoutes= require("./routes/authRoutes")
+const sessionRoutes= require("./routes/sessionRoutes")
 
 const app = express()
 const PORT= 3010
@@ -13,9 +18,17 @@ const PORT= 3010
 
 app.use(express.json()) // Invocamos al middleware para que parsee los datos del body de las solicitudes en formato JSON.
 
-// Rutas
+// Rutas de Autenticacion
+
+app.use("/api/auth", authRoutes)
+
+// Rutas de Usuarios
 
 app.use("/api/users", userRoutes) // Creamos las rutas de usuario en la ruta /api/users
+
+// Rutas del usuario actual
+
+app.use("/api/session", sessionRoutes)
 
 // Iniciamos la db.
 
